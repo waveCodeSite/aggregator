48a458034b7a515f778ecf69fc5d91795dff781c
wave <jxdyxjt@163.com>
2026-08-09 19:07:23 +0800

    update

--- subscribe/airport.py
+++ subscribe/airport.py
@@ -762,7 +762,7 @@ class AirPort:
                 if os.path.exists(v2ray_file):
                     os.remove(v2ray_file)
 
-                logger.error(f"save file fialed, artifact: {artifact}")
+                logger.error(f"save file failed, artifact: {artifact}")
                 traceback.print_exc()
 
             generate_conf = os.path.join(PATH, "subconverter", "generate.ini")
